347e440f5e5931f0c77db14f175e14bdb2b42faa
mmhuntsberry <mmhuntsberry@gmail.com>
2024-10-08 10:54:55 -0500

    revert image block

--- apps/portfolio/sanity/schemas/study-schema.ts
+++ apps/portfolio/sanity/schemas/study-schema.ts
@@ -47,21 +47,7 @@ const study = {
       type: "array",
       of: [
         { type: "block" },
-        {
-          name: "image",
-          title: "Image",
-          type: "image",
-          options: {
-            hotspot: true,
-          },
-          fields: [
-            {
-              name: "alt",
-              title: "Alt",
-              type: "string",
-            },
-          ],
-        },
+        { type: "image" },
         {
           type: "object",
           name: "section",
